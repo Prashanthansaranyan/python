51132de1001ecb26547491249ca09ddd7574c00a
Prashanthansaranyan <ssaranyan422@gmail.com>
2026-02-09 11:58:51 +0530

    basic

---
+++ python2.py
@@ -0,0 +1,11 @@
+fname="saran"
+lname="yan"
+gmail="saran112@gmail.com"
+salary=100000
+print("*****************")
+print("Employee details")
+print("*****************")
+print("Full name    :"+fname+lname)
+print("Gmail        :"+gmail)
+print("Basic salary :"+str(salary))
+print("***************")
