c2c5ecf3bdb2684f0526be5ccc92dc361bbd3dba
Prashanthansaranyan <ssaranyan422@gmail.com>
2026-02-10 11:01:09 +0530

    user input

---
+++ userinput.py
@@ -0,0 +1,20 @@
+"""name=input("Enter your name ")
+print("your name is:"+name)
+print("your age is:",age)
+print("*****************")"""
+"""print("your name is:"+name)
+print("your age is:"+str(age))
+print("*****************")"""
+"""num1= int( input("Enter the number1 "))
+num2=int (input("Enter the number2 "))
+num3=num1+num2
+print("Total:"+str(num3))
+print("****************")"""
+"""(num1=  input(int("Enter the number1 "))\\ it is not work
+num2=input(int ("Enter the number2 "))
+num3=num1+num2
+print("Total:"+str(num3)))"""
+"""fname-lname=10
+print(fname-lname)
+
+
