64cfad0fecbb788cdd139d41785ebbf26c70f609
Prashanthansaranyan <ssaranyan422@gmail.com>
2026-02-11 14:44:43 +0530

    python

---
+++ Conditionswork1.py
@@ -0,0 +1,5 @@
+y=int(input("Enter your age"))
+if y>=18:
+	print("eligiblity of voting")
+else:
+	print("not eligiblity of voting")
---
+++ LogicalOperators.py
@@ -0,0 +1,12 @@
+"""x=True
+y=False
+print("x&y : ",x and y)
+print("x or y : ",x or y)
+print("not x : ",not x)"""
+x=10
+y=20
+print("x=",x)
+print("y=",y)
+print("x>y and x<30 : ",x>y and x<30)
+print("x>y or y>5 : ",x>y or y>5)
+print("not (x==y) :",not (x==y))
