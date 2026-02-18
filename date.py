22b0c3708fd957e0d1429bae0b84e6e130ec0e04
Prashanthansaranyan <ssaranyan422@gmail.com>
2026-02-18 11:15:25 +0530

    python

---
+++ date.py
@@ -0,0 +1,73 @@
+day1="monday"
+day2="tuesday"
+day3="wednesday"
+day4="thursday"
+day5="friday"
+day6="saturday"
+day7="sunday"
+day=int(input("which date you want : "))
+if 0<day<=31:
+    if day==1:
+        print("today is : ",day1)
+    elif day==2:
+        print("today is : ",day2)
+    elif day==3:
+        print("today is : ",day3)
+    elif day==4:
+        print("today is : ",day4)
+    elif day==5:
+        print("today is : ",day5)
+    elif day==6:
+        print("today is : ",day6)
+    elif day==7:
+        print("today is : ",day7)
+    elif day==8:
+        print("today is : ",day1)
+    elif day==9:
+        print("today is : ",day2)
+    elif day==10:
+        print("today is : ",day3)
+    elif day==11:
+        print("today is : ",day4)
+    elif day==12:
+        print("today is : ",day5)
+    elif day==13:
+        print("today is : ",day6)
+    elif day==14:
+        print("today is : ",day7)
+    elif day==15:
+        print("today is : ",day1)
+    elif day==16:
+        print("today is : ",day2)
+    elif day==17:
+        print("today is : ",day3)
+    elif day==18:
+        print("today is : ",day4)
+    elif day==19:
+        print("today is : ",day5)
+    elif day==20:
+        print("today is : ",day6)
+    elif day==21:
+        print("today is : ",day7)
+    elif day==22:
+        print("today is : ",day1)
+    elif day==23:
+        print("today is : ",day2)
+    elif day==24:
+        print("today is : ",day3)
+    elif day==25:
+        print("today is : ",day4)
+    elif day==26:
+        print("today is : ",day5)
+    elif day==27:
+        print("today is : ",day6)
+    elif day==28:
+        print("today is : ",day7)
+    elif day==29:
+        print("today is : ",day1)
+    elif day==30:
+        print("today is : ",day2)
+    else:
+        print("today is : ",day3)
+else:
+    print("invalid!")
