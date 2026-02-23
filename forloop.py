00b7b85380d609ac70da80b9d1ce49c734ae80a4
Prashanthansaranyan <ssaranyan422@gmail.com>
2026-02-23 13:11:08 +0530

    python

---
+++ forloop.py
@@ -0,0 +1,45 @@
+"""for x in range(1,5):
+	print(x)
+print("************ ")    
+for x in range(5):
+	print(x)"""
+    
+for x in range(1,10,2):
+    print(x)
+    
+print("*******************")
+
+for x in range(10,0,-1):
+	print(x)
+    
+print("*********************")
+
+for x in range(10):
+    if x%2 ==0:
+        print("even number : ",x)
+    else:
+        print("odd number : ",x)
+        
+print("**************************")
+
+"""for x in True:
+    print(x)
+    
+print("**************************")"""
+
+for x in "yarlit":
+    print(x)
+    
+print("**************************")
+
+for x in range (10):
+    if (x==5):
+        break
+    print(x)
+        
+print("**************************")
+
+for x in range (10):
+    if (x==5):
+        continue
+    print(x)
