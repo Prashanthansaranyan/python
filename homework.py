3567e4154a5820b487240e096d8c164862f23110
Prashanthansaranyan <ssaranyan422@gmail.com>
2026-02-27 13:18:57 +0530

    python

---
+++ homework.py
@@ -0,0 +1,51 @@
+for x in range(6):
+    if x%2 ==1:
+        print(x," ",x," ",x," ",x)
+    else:
+        print("* * * * ")
+print("*************************************")        
+for x in range(5):
+    for i in range(5,0,-1):
+        print(i,end="")
+    print()
+print("*************************************")     
+for x in range(1,6):
+    for i in range(5):
+        print(x,end="")
+    print()
+    
+print("*************************************")   
+for x in range(2,10):
+    if x%2 ==1:
+        print(x,end=" ")
+print("*************************************") 
+for x in range(1,6):
+    sum = x ** 2
+    print(sum,end="  ")
+print("*************************************")     
+for x in range(1,5):
+    sum = x*(x+1)/2
+    print(sum,end="  ")
+    
+print("*************************************")     
+for x in range(1,6):
+    if x%2==1:
+        print(x,end="")
+    else:
+        print("*",end="")
+        
+print("*************************************") 
+
+for x in range(2,10):
+    if x%2 ==0:
+        print(x,end=" ")
+        
+for x in range(1,6):
+    for i in range(1,x+1):
+        print(i,end="")
+    print()
+
+    
+    
+
+    
